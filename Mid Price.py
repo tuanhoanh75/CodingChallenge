bd466d5abeeaa701f100b934c1ea7bb01d23dfe6
Tuan Ho Anh <tuanhoanh75@gmx.de>
2020-10-06 16:24:26 +0200

    Code Challenge: Task: Mid Price - DONE

--- Mid Price.py
+++ Mid Price.py
@@ -1,9 +1,15 @@
-def checkInputBid(bid, mid):
-    i = 1
-    while (i > bid) and (bid < mid):
-        print("\nYour bid price must be greater or equal to one!")
-        bid = int(input("Re-enter your bid price: "))
-    return bid
+def checkInput(mid, bid):
+    if 1 <= bid < mid <= 1000:
+        return mid, bid
+    else:
+        print("\nWarning: One of your entered input value is violating the given limitation.")
+        print("Please check and correct you input price!\n")
+
+        new_mid = int(input("Re-enter your mid price: "))
+        new_bid = int(input("Re-enter your bid price: "))
+        res = checkInput(new_mid, new_bid)
+
+        return res
 
 
 def calculatePrice(midPrice, bidPrice):
@@ -15,9 +21,12 @@ if __name__ == '__main__':
 
     print("------------------------------------------------------------")
     print("Please enter your mid/bid price to calculate the ask price.")
-    print("Note: 1 <= bidPrice < midPrice <= 1000 ")
+    print("Note: 1 <= bidPrice < midPrice <= 1000")
     print("------------------------------------------------------------\n")
 
-    mid = int(input("First, enter your mid price: "))
-    bid = int(input("Second, enter your bid price: "))
-    checkInputBid(bid, mid)
+    mid = int(input("FIRST, enter your mid price: "))
+    bid = int(input("\nSECOND, enter your bid price: "))
+    check = checkInput(mid, bid)
+
+    ask = calculatePrice(mid, bid)
+    print("\nThe calculated ask price is: ", ask)
