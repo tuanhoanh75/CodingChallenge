d328589e186e0e106fbc559c0150933741779c5e
Tuan <tuanhoanh75@gmx.de>
2020-10-15 15:22:11 +0200

    Approach for profit function

--- Testing.py
+++ Testing.py
@@ -1,14 +1,44 @@
-while True:
-    try:
-        print("\nTo enter a youth club in groups, it is mandatory to verify the age of each person.\n"
-              "The age restriction: From 17 to 21.\n"
-              "If at least one person from the group is a minor, then admission will be denied!\n"
-              "----------------------------------------------------------------------------------")
+def buy_and_sell(stock_price):
+    n = len(stock_price)
 
-        age = list(map(int, input("\nThe list of age separated by space: ").strip().split()))
-        check = all(17 <= i <= 21 for i in age)
-        print("Return: ", check)
-    except Exception as e:
-        print(e)
-    else:
-        exit(0)
+    # Create profit array and initialize it as 0
+    profit = [0]*n
+
+    # Get the maximum profit with only one tx allowed
+    # After this loop. profit[i] contains maximum
+    # profit from price[i...n-1] using at most one tx.
+    max_price = stock_price[n - 1]
+
+    for i in range(n-2, 0, -1):
+
+        if stock_price[i] > max_price:
+            max_price = stock_price[i]
+
+        # we can get profit[i] by taking maximum of:
+        # a) previous maximum, i.e., profit[i+1]
+        # b) profit by buying at price[i] and selling at max_price
+        profit[i] = max(profit[i+1], max_price - stock_price[i])
+
+    # Get the maximum profit with two tx allowed
+    # After this loop, profit[n-1] contains the result
+    min_price = stock_price[0]
+
+    for i in range(1, n):
+        if stock_price[i] < min_price:
+            min_price = stock_price[i]
+
+        # Maximum profit is maximum of:
+        # a) previous maximum, i.e., profit[i-1]
+        # b) (Buy, Sell) at (min_price, A[i]) and add
+        #    profit of other tx stored in profit[i]
+        profit[i] = max(profit[i-1], profit[i] + (stock_price[i] - min_price))
+
+    result = profit[n-1]
+    return result
+
+
+if __name__ == '__main__':
+    stock_price = list(map(int, input("\nEnter your list of stock prices: ").strip().split()))
+
+    print("Max. profit: ", buy_and_sell(stock_price))
+
