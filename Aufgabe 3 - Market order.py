6f66d46ec8239c105708be444503b957a75d59fb
Tuan <tuanhoanh75@gmx.de>
2020-10-21 14:43:49 +0200

    Task 3: Market order

---
+++ Aufgabe 3 - Market order.py
@@ -0,0 +1,30 @@
+def check_input():
+    while True:
+        try:
+            volume = list(map(int, input("Enter your order volumes: ").strip().split()))
+            vol_size = len(volume) in range(100)
+            numb_limit = all(i in range(2, 51) for i in volume)
+
+            if numb_limit and vol_size:
+                print("\nvolumes =", volume)
+            else:
+                print("\nAttention: The order list doesn't meet the given requirements! Please try again.\n")
+                continue
+        except Exception as e:
+            print(e, "\n")
+        else:
+            return volume
+
+
+def countTraders(volumes):
+    pass
+
+
+if __name__ == '__main__':
+    print("----------------------------------------------------------------------------")
+    print("Please enter the order volumes.")
+    print("Limitation: A) There are at most 100 order volumes on market")
+    print("            B) Each volume is an integer number between 2 and 50 inclusive")
+    print("----------------------------------------------------------------------------\n")
+
+    volumes = check_input()
