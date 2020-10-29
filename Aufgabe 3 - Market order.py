3b57dbe8eee9ee6fbeb24134de7c48854465047c
Tuan <tuanhoanh75@gmx.de>
2020-10-29 16:13:05 +0100

    Implement two for loops

--- Aufgabe 3 - Market order.py
+++ Aufgabe 3 - Market order.py
@@ -6,9 +6,9 @@ def check_input():
             numb_limit = all(i in range(2, 51) for i in volume)
 
             if numb_limit and vol_size:
-                print("\nvolumes =", volume)
+                print("\nYour given volumes =", volume, "\n")
             else:
-                print("\nAttention: The order list doesn't meet the given requirements! Please try again.\n")
+                print("Attention: The order list doesn't meet the given requirements! Please try again.\n")
                 continue
         except Exception as e:
             print(e, "\n")
@@ -17,9 +17,24 @@ def check_input():
 
 
 def countTraders(volumes):
-    # Set number of passes/runs
-    runs = range(2, max(volumes)+1)
-    pass
+    if not volumes:
+        return 0
+
+    n_trader = 0
+    vol = []
+
+    for i in range(2, max(volumes)+1):
+        x = i
+        print("Current X =", x)
+
+        set = []
+        for j in volumes:
+            if (j % x) == 0:
+                set.append(j)
+
+        print("Subset =", set)
+
+
 
 
 if __name__ == '__main__':
@@ -31,4 +46,4 @@ if __name__ == '__main__':
 
     volumes = check_input()
 
-    print("traders =", countTraders(volumes))
+    print("\ntraders =", countTraders(volumes))
